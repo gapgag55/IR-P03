e0e96e4dca97a403f921d1ee838ebe7017ad1227
Kopkap <kopkap@Kopkaps-MacBook-Pro-273.local>
2018-11-28 23:08:07 +0700

    1.0

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Form, Icon, Input, Button } from 'antd';
+import "typeface-roboto";
+// import 'antd/dist/antd.css';
 import './App.css';
 
-class App extends Component {
+const FormItem = Form.Item;
+
+class App extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container flex-column">
+        <div className="bg"></div>
+        <h1 className="headline">PANTIP LISTENING</h1>
+        <Form onSubmit={this.handleSubmit} className="search-form">
+          <FormItem>
+            {getFieldDecorator('keyword')(
+              <Input suffix={<Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Keyword" size="large" />
+            )}
+          </FormItem>
+          <FormItem>
+            <div className="flex-between">
+              <Button type="primary" htmlType="submit" size="large" className="search-button">
+                Search Sentiment
+              </Button>
+              <Button type="primary" htmlType="submit" size="large" className="search-button">
+                Search Question
+              </Button>
+            </div>
+          </FormItem>
+        </Form>
       </div>
     );
   }
 }
 
-export default App;
+export default Form.create()(App);
